b72943890608827830b54375f587cd912f755f15
Daria Korniienko <106033233+Dashasoham@users.noreply.github.com>
2023-01-06 22:36:13 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let hasDriversLicense = false
+const passTest = true
+
+if (passTest) hasDriverLicense = true
+if (hasDriversLicense) console.log("I can drive")
+
+
+//reserved words
+// const interface="Audio"
+// const private = 32
+const kol = 67
