8af715c50533e3761a36668cd6c32667841d2a77
Daria Korniienko <106033233+Dashasoham@users.noreply.github.com>
2023-01-07 15:28:41 +0000

    arrow f-s

---
+++ assignments.js
@@ -0,0 +1,61 @@
+"use strict";
+
+
+/*
+function describeCountry(country, popuplation, capitalCity) {
+    const sentence = `${country} has ${popuplation} million people and its capital city is ${capitalCity}`
+    return sentence
+}
+
+const finland = describeCountry("Finland", 6, "Helsinki")
+const ukraine = describeCountry("Ukraone", 38, "Kyiv")
+const portugal = describeCountry("Portugal", 10, "Lisbon")
+
+
+console.log(finland)
+console.log(ukraine)
+console.log(portugal)
+
+
+//function declarations and expressions
+
+const worldPopulation = 7900
+
+function percentageOfWorld(country, population) {
+    const populationByCountry = (population / worldPopulation) * 100
+    const percentage = `${country}´s population is ${populationByCountry}% of the world´s population`
+
+    return percentage
+}
+
+const chinaPopulation = percentageOfWorld("China", 1441)
+const portugalPopulation = percentageOfWorld("Portugal", 10)
+const ukrainePopulation = percentageOfWorld("Ukraine", 44)
+
+
+console.log(chinaPopulation, portugalPopulation, ukrainePopulation)
+
+const percentageOfTheWorld = function (country, population) {
+    const populationByCountry = (population / worldPopulation) * 100
+    const percentage = `${country}´s population is ${populationByCountry}% of the world´s population`
+
+    return percentage
+}
+
+
+const chinaPopulation2 = percentageOfWorld("China", 1441)
+const portugalPopulation2 = percentageOfWorld("Portugal", 10)
+const ukrainePopulation2 = percentageOfWorld("Ukraine", 44)
+
+
+console.log(chinaPopulation2, portugalPopulation2, ukrainePopulation2)
+*/
+
+const percentageOfTheWorld3 = population => (population / 7900) * 100
+
+//const populationCina3 = percentageOfTheWorld3(1440)
+//console.log(populationChina3)
+
+console.log(percentageOfTheWorld3(1440))
+console.log(percentageOfTheWorld3(42))
+console.log(percentageOfTheWorld3(10))
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-"use strict";
 
+"use strict";
+/*
 let hasDriversLicense = false
 const passTest = true
 
@@ -10,3 +11,65 @@ if (hasDriversLicense) console.log("I can drive")
 //reserved words
 // const interface="Audio"
 // const private = 32
+
+
+function logger() {
+    console.log("My name is Dasha")
+}
+//calling, running or invoking the function
+logger()
+logger()
+
+function fruitProcessor(apples, oranges) {
+    //console.log(apples, oranges)
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`
+    return juice
+}
+const appleJuice = fruitProcessor(5, 0)
+console.log(appleJuice)
+
+const appleOrangeJuice = fruitProcessor(2, 4)
+console.log(appleOrangeJuice)
+
+//function declaration
+
+const age1 = calcAge1(1991)
+
+function calcAge1(birthYear) {
+    return 2037 - birthYear
+}
+
+console.log(age1)
+
+
+//function expressions, anonymous function
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear
+
+}
+
+const age2 = calcAge2(1991)
+console.log(age1, age2)
+
+
+
+
+
+const calcAge3 = birthYear => 2037 - birthYear
+
+//const age3 = calcAge3(1991)
+//console.log(age3)
+
+console.log(calcAge3(1991))
+
+const yearsUntilRet = (birthYear, firstName) => {
+    const age = 2037 - birthYear
+    const retirement = 65 - age
+    //return retirement
+    return `${firstName} retires in ${retirement} years `
+}
+
+console.log(yearsUntilRet(1991, "John"))
+console.log(yearsUntilRet(1980, "Bob"))
+
+*/
